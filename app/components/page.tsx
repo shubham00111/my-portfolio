1499a267b0f4869778bc7a7b0a7d12f936e6ad1e
Shubham Rawat <shubhamb23efd@gmail.com>
2025-09-15 20:42:44 +0530

    made the second section of my portfolio

---
+++ app/components/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import headerLogo from "@/app/assets/header-logo-light.svg";
+
+const page = () => {
+  return <div>fdjaslfj</div>;
+};
+
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import Header from "@/components/Header";
-
+import headerLogo from "@/app/assets/header-logo-light.svg";
+import Image from "next/image";
 export default function Home() {
-  return <h2>fjdlsajflaj</h2>;
+  return (
+    <div className="grid grid-cols-[1fr_2fr_1fr] border-b-1">
+      <div />
+      <section className="flex justify-center p-10 bg-[radial-gradient(var(--pattern-foreground)_1px,transparent_0)] bg-size-[10px_10px] bg-center border-x-1 border-x-[var(--border) bg-size-[10px_10px] ">
+        <Image src={headerLogo} alt="" width={200} className="rounded-full" />
+      </section>
+      <div />
+    </div>
+  );
 }
