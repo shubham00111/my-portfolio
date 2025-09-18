6c69f3871decb4821c56838d111206864a66f384
Shubham Rawat <shubhamb23efd@gmail.com>
2025-09-18 12:36:20 +0530

    added the profile_pic section with description

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,21 @@ export default function Portfolio() {
           <Image src={headerLogo} alt="" className="rounded-full" />
         </div>
       </div>
-      <div className="max-w-3xl mx-auto screen-line-before border-x-1 border-x-[var(--border)]">
-        <div className="md:w-45">
+      <div className="max-w-3xl flex mx-auto screen-line-before border-x-1 border-x-[var(--border)]">
+        <div className="border-r md:w-45">
           <Image
             src={profileImage}
             alt="profile_pic"
-            className="rounded-full"
+            className="rounded-full ring ring-border ring-offset-2"
           />
         </div>
+        <div className="flex flex-1 flex-col ">
+          <div className="grow bg-[repeating-linear-gradient(315deg,var(--pattern-foreground)_0,var(--pattern-foreground)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px] "></div>
+          <div className="border-t text-2xl p-2 font-bold">Shubham Rawat</div>
+          <div className="p-2 border-t text-gray-600">Front End Developer</div>
+        </div>
       </div>
+      <div></div>
     </div>
   );
 }
