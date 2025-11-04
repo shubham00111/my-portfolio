3addcc3153a53df9a85266203356c025d2b33e57
Shubham Rawat <shubhamb23efd@gmail.com>
2025-11-05 01:34:11 +0530

    added spinner for graph loading

--- components/ContributionSection.tsx
+++ components/ContributionSection.tsx
@@ -6,6 +6,7 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "./ui/tooltip";
+import { LoaderIcon } from "lucide-react";
 import {
   ContributionGraph,
   ContributionGraphBlock,
@@ -16,7 +17,7 @@ import {
 } from "./kibo-ui/contribution-graph";
 import { eachDayOfInterval, endOfYear, formatISO, startOfYear } from "date-fns";
 import SectionContainer from "./section/SectionContainer";
-import SectionTitle from "./section/SectionTitle";
+import { Spinner } from "./ui/spinner";
 import SectionContent from "./section/SectionContent";
 
 const ContributionSection = () => {
@@ -35,57 +36,64 @@ const ContributionSection = () => {
   return (
     <SectionContainer>
       <SectionContent className="screen-line-before px-3 py-2">
-        <TooltipProvider>
-          <ContributionGraph
-            data={data}
-            blockMargin={3}
-            blockSize={11}
-            fontSize={14}
-            blockRadius={0}
-          >
-            <ContributionGraphCalendar
-              className="no-scrollbar pt-2"
-              title="Github Contributions"
+        {data.length > 0 ? (
+          <TooltipProvider>
+            <ContributionGraph
+              data={data}
+              blockMargin={3}
+              blockSize={11}
+              fontSize={14}
+              blockRadius={0}
             >
-              {({ activity, dayIndex, weekIndex }) => (
-                <Tooltip>
-                  <TooltipTrigger asChild>
-                    <g>
-                      <ContributionGraphBlock
-                        activity={activity}
-                        dayIndex={dayIndex}
-                        weekIndex={weekIndex}
-                      />
-                    </g>
-                  </TooltipTrigger>
-                  <TooltipContent>
-                    <p className="font-semibold">{activity.date}</p>
-                    <p>{activity.count} contributions</p>
-                  </TooltipContent>
-                </Tooltip>
-              )}
-            </ContributionGraphCalendar>
-            <ContributionGraphFooter>
-              <ContributionGraphTotalCount>
-                {({ totalCount, year }) => (
-                  <div className="text-muted-foreground">
-                    {totalCount.toLocaleString("en")} contributions in {year} on{" "}
-                    <a
-                      className="font-medium underline underline-offset-4"
-                      href={`https://github.com/${"shubham00111"}`}
-                      target="_blank"
-                      rel="noopener"
-                    >
-                      GitHub
-                    </a>
-                    .
-                  </div>
+              <ContributionGraphCalendar
+                className="no-scrollbar pt-2"
+                title="Github Contributions"
+              >
+                {({ activity, dayIndex, weekIndex }) => (
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <g>
+                        <ContributionGraphBlock
+                          activity={activity}
+                          dayIndex={dayIndex}
+                          weekIndex={weekIndex}
+                        />
+                      </g>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p className="font-semibold">{activity.date}</p>
+                      <p>{activity.count} contributions</p>
+                    </TooltipContent>
+                  </Tooltip>
                 )}
-              </ContributionGraphTotalCount>
-              <ContributionGraphLegend />
-            </ContributionGraphFooter>
-          </ContributionGraph>
-        </TooltipProvider>
+              </ContributionGraphCalendar>
+              <ContributionGraphFooter>
+                <ContributionGraphTotalCount>
+                  {({ totalCount, year }) => (
+                    <div className="text-muted-foreground">
+                      {totalCount.toLocaleString("en")} contributions in {year}{" "}
+                      on{" "}
+                      <a
+                        className="font-medium underline underline-offset-4"
+                        href={`https://github.com/${"shubham00111"}`}
+                        target="_blank"
+                        rel="noopener"
+                      >
+                        GitHub
+                      </a>
+                      .
+                    </div>
+                  )}
+                </ContributionGraphTotalCount>
+                <ContributionGraphLegend />
+              </ContributionGraphFooter>
+            </ContributionGraph>
+          </TooltipProvider>
+        ) : (
+          <div className="size- flex h-30 items-center justify-center">
+            <Spinner className="size-8" />
+          </div>
+        )}
       </SectionContent>
     </SectionContainer>
   );
